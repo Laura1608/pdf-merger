0b88c9176cad18f91420f43d96c25a7b7749d914
Laura1608 <131393433+Laura1608@users.noreply.github.com>
2024-04-30 11:31:26 +0200

    Project upload

---
+++ pdf-merger.py
@@ -0,0 +1,23 @@
+from PyPDF2 import PdfWriter
+import os
+import glob
+
+# Set filepath
+filepath = 'C:/Users/laura/Downloads'
+os.chdir(filepath)
+
+# Define merger
+merger = PdfWriter()
+
+# Create for loop where file type = pdf
+for file in glob.glob("*.pdf"):
+    # File name has to contain a certain word
+    if file.__contains__('factuur'):
+        # Add those matching files to a new combined file
+        merger.append(file)
+        merger.write('combined_file.pdf')
+        merger.close()
+        print("Merging successful!")
+    else:
+        # Show me a message when files didn't match
+        print("File doesn't match requirements.")
